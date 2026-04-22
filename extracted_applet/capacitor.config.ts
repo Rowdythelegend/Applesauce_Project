edd52606ededc1a202b5dadd261a1031fa43c397
Ndumiso (The Clairvoyant) <nslegend8@gmail.com>
2026-04-22 20:40:07 +0200

    feat: setup capacitor and github actions for remote android build

---
+++ extracted_applet/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.bambanatech.applesauce',
+  appName: "Kristel's Genius Hub",
+  webDir: 'dist'
+};
+
+export default config;
